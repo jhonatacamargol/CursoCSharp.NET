a4285484a3f2d0838d530659069c9f7801d8eae1
jhonatacamargol <jhonatapires32@hotmail.com>
2022-11-14 14:07:02 -0300

    commit course

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //Fundamentos
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentarios - Fundamentos", Comentarios.Executar},
+                {"Variaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferência - Fundamentos", inferencia.Executar},
+                {"Interpolacao - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Número - Fundamentos", FormatandoNumero.Executar},
+                {"Conversões - Fundamentos", Conversoes.Executar},
+                {"Operadores Aritiméticos - Fundamentos", OperadoresAritimeticos.Executar},
+                {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
+                {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+                
+                //Estruturas de Controle
+                {"Estrutura IF - Estruturas de Controle", EstruturaIF.Executar},
+                {"Estrutura IF/Else - Estruturas de Controle", EstruturaIfElse.Executar},
+                {"Estrutura IF/Else/If - Estruturas de Controle", EstruturaIfElseIf.Executar},
+                {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
+
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
