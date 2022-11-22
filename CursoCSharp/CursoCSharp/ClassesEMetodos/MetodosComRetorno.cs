3ddd65cfdf05ecb999a65b6c44a3768b15248e8f
jhonatacamargol <jhonatapires32@hotmail.com>
2022-11-21 21:40:38 -0300

    Update MetodosComRetorno.cs

--- CursoCSharp/CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -20,6 +20,35 @@ namespace CursoCSharp.ClassesEMetodos
             return a / b;
         }
     }
+
+    class CalculadoraCadeia {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a) {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a) {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar() {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado () {
+            return memoria;
+        }
+    }
+
     internal class MetodosComRetorno
     {
         public static void Executar() {
@@ -30,6 +59,9 @@ namespace CursoCSharp.ClassesEMetodos
             Console.WriteLine(CalculadoraComum.Subitrair(14, 5));
             Console.WriteLine(CalculadoraComum.Multiplicar(18, 36));
             Console.WriteLine(CalculadoraComum.Dividir(526, 8));
+
+            var CalculadoraCadeia = new CalculadoraCadeia();
+            CalculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
         }
     }
 }
