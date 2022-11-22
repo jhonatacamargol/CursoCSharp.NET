457e99246f8da9d73afb25b44f6e6a11f20c5b22
jhonatacamargol <jhonatapires32@hotmail.com>
2022-11-21 21:04:47 -0300

    atualizar.

---
+++ CursoCSharp/CursoCSharp/EstruturasDeControle/EstruturaIF.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    internal class EstruturaIF
+    {
+        public static void Executar() {
+            bool bomComportamento = false;
+            string entrada;
+
+            Console.WriteLine("Digite a nota do aluno: ");
+            entrada = Console.ReadLine();
+            Double.TryParse(entrada, out double nota);
+
+            Console.WriteLine("Possui bom comportamento (S/N): ");
+            entrada = Console.ReadLine();
+
+            //if (entrada == "s" || entrada == "S")
+            //  bomComportamento = true;
+
+            // bomComportamento = entrada == "s" || entrada == "S";
+            bomComportamento = entrada.ToLower() == "s";
+            
+            if(nota >= 9.0 && bomComportamento){
+                Console.WriteLine("Quadro de honrra!");
+            }
+        }
+    }
+}
---
+++ CursoCSharp/CursoCSharp/Fundamentos/OperadorTernario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadorTernario
+    {
+        public static void Executar() {
+            var nota = 9.0;
+            bool BomComportamento = false;
+            string resultado = nota >= 7.0 && BomComportamento ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+        }
+    }
+}
