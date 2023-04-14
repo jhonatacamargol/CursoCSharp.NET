51e181e1659830d1921df34b143197bc9edbe7dc
jhonatacamargol <jhonatapires32@hotmail.com>
2023-04-14 12:21:02 -0300

    atualizar

---
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/ExemploEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public enum Genero { Acao, Aventura, Terror, Animacao, Comedia};
+
+    public class Filme
+    {
+        public string Titulo;
+        public Genero GeneroDoFilme;
+    }
+    internal class ExemploEnum
+    {
+        public static void Executar()
+        {
+            int id = (int)Genero.Animacao;
+            Console.WriteLine(id);
+
+            var filmeParaFamililia = new Filme();
+            filmeParaFamililia.Titulo = "Sharknardo 17";
+            filmeParaFamililia.GeneroDoFilme = Genero.Comedia;
+
+            Console.WriteLine("{0} é {1}!", filmeParaFamililia.Titulo, filmeParaFamililia.GeneroDoFilme);
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -49,6 +49,8 @@ namespace CursoCSharp {
                 {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
                 {"Get Set - Classes e Métodos", GetSet.Executar},
                 {"Props - Classes e Métodos", Props.Executar},
+                {"Readonly - Classes e Métodos", Readonly.Executar},
+                {"ExemploEnum - Classes e Métodos", ExemploEnum.Executar},
             }) ;
 
             central.SelecionarEExecutar();
